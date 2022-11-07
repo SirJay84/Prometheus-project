250de0f76a1481d0a8e2eb9860cde2665cc046c7
Jimmy <jimbotela@gmail.com>
2022-11-07 21:20:59 +0300

    first commit

---
+++ http_server_app.py
@@ -0,0 +1,16 @@
+import http.server
+from prometheus_client import start_http_server
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+  def do_GET(self):
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/html')
+    self.end_headers()
+    self.wfile.write(bytes("<html><head><title></title></head><body style='color:#333;margin-top:30px;'><center><h2>Welcome to Prometheus-Python application</center></h2></html>","utf-8"))
+    self.wfile.close
+
+if  __name__ == '__main__':
+  # Start up the server to expose the metrics.
+  start_http_server(5001)
+  server = http.server.HTTPServer(('localhost',5000), HandleRequests)
+  server.serve_forever()
