7c3dd7b08b2ea9a1ef7a02d6d5a519e15d66171a
Jimmy <jimbotela@gmail.com>
2022-11-09 16:16:28 +0300

    created counter_metric.py - to check total http request count

---
+++ counter_metric.py
@@ -0,0 +1,19 @@
+import http.server
+from prometheus_client import start_http_server, Counter 
+
+REQUEST_COUNT = Counter('app_request_counts', 'Total HTTP Request Count')
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+  def do_GET(self):
+    REQUEST_COUNT.inc()
+    self.send_response(200)
+    self.send_header('Content-Type', 'text/html')
+    self.end_headers()
+    self.wfile.write(bytes("<html><head><title></title></head><body style='color:#333;margin-top:30px;'><center><h2>Welcome to Prometheus-Python application</center></h2></html>","utf-8"))
+    self.wfile.close
+
+if  __name__ == '__main__':
+  # Start up the server to expose the metrics.
+  start_http_server(5001)
+  server = http.server.HTTPServer(('134.122.70.44',5000), HandleRequests)
+  server.serve_forever()
