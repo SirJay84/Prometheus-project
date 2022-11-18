a0f83640da95eabbd8abffebaa39e1017ceee4e2
Jimmy <jimbotela@gmail.com>
2022-11-18 16:53:37 +0300

    additional code on  gauge_metric.py

--- gauge_metric.py
+++ gauge_metric.py
@@ -6,16 +6,18 @@ REQUEST_IN_PROGRESS = Gauge('request_in_progress', 'Number of Live Request on Ap
 REQUEST_LAST_EXECUTED = Gauge('request_last_served', 'Time the Application was last served')
 
 class HandleRequests(http.server.BaseHTTPRequestHandler):
+  @REQUEST_IN_PROGRESS.track_inprogress()
   def do_GET(self):
-    REQUEST_IN_PROGRESS.inc()
+    # REQUEST_IN_PROGRESS.inc()
     time.sleep(5)
     self.send_response(200)
     self.send_header('Content-Type', 'text/html')
     self.end_headers()
     self.wfile.write(bytes("<html><head><title></title></head><body style='color:#333;margin-top:30px;'><center><h2>Welcome to Prometheus-Python application</center></h2></html>","utf-8"))
     self.wfile.close
-    REQUEST_LAST_EXECUTED.set(time.time())
-    REQUEST_IN_PROGRESS.dec()
+    REQUEST_LAST_EXECUTED.set_to_current_time()
+    # REQUEST_LAST_EXECUTED.set(time.time())
+    # REQUEST_IN_PROGRESS.dec()
 
 if  __name__ == '__main__':
   # Start up the server to expose the metrics.
