1b2e577d4b1e056d4ca0ee2d3ab1d77e56d27951
Jimmy <jimbotela@gmail.com>
2022-11-18 20:24:15 +0300

    created histogram_metric.py - calculate latency time for particular request

---
+++ histogram_metric.py
@@ -0,0 +1,22 @@
+import http.server
+import time
+from prometheus_client import start_http_server, Histogram
+
+REQUEST_LATENCY_TIME = Histogram('request_latency_time', 'Response latency time in seconds')
+
+class HandleRequests(http.server.BaseHTTPRequestHandler):
+  @REQUEST_LATENCY_TIME.time()
+  def do_GET(self):
+    self.send_response(200)
+    time.sleep(1)
+    self.send_header('Content-Type', 'text/html')
+    self.end_headers()
+    self.wfile.write(bytes("<html><head><title></title></head><body style='color:#333;margin-top:30px;'><center><h2>Welcome to Prometheus-Python application</center></h2></html>","utf-8"))
+    self.wfile.close
+    
+
+if  __name__ == '__main__':
+  # Start up the server to expose the metrics.
+  start_http_server(5001)
+  server = http.server.HTTPServer(('134.122.70.44',5000), HandleRequests)
+  server.serve_forever()
